c9dd568650776a6e55987db18c88378d18a3196b
Samridh Gupta <samridhgupta1053@gmail.com>
2024-01-31 12:15:52 +0530

    [samridh] added: updated note.service.js file

--- src/services/note.service.js
+++ src/services/note.service.js
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 
 //create new user
 export const newNote = async (body) => {
- 
+
   const data = await Note.create(body);
   return data;
 };
@@ -14,26 +14,28 @@ export const newNote = async (body) => {
 //get single user
 export const getNote = async (body) => {
   const data = await Note.findOne({title: body.title});
- 
   return data;
 };
 
 export const getAllNotes = async (body) => {
   const data = await Note.find();
- 
   return data;
 };
 
-export const updateNote = async (body) => {
-  const data = await Note.findByIdAndUpdate(body);
- 
+export const updateNote = async (_id, body) => {
+  const data = await Note.findByIdAndUpdate(    {
+    _id
+  },
+  body,
+  {
+    new: true
+  });
   return data;
 };
 
-export const deleteNote = async (body) => {
-  const data = await Note.findByIdAndDelete({title: body.title});
- 
-  return data;
+export const deleteNote = async (id) => {
+  await Note.findByIdAndDelete(id);
+  return '';
 };
 
 
